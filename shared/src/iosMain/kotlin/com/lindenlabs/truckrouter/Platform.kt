2fde80ee0ad698b426b47a2472a6e803595e5750
Abass Bayo <abass@Abasss-MBP.lan>
2023-02-26 18:52:53 -0500

    Further cleanup

--- shared/src/iosMain/kotlin/com/lindenlabs/truckrouter/Platform.kt
+++
@@ -1,8 +0,0 @@
-package com.lindenlabs.truckrouter
-
-import com.lindenlabs.truckrouter.data.AppDataSource
-
-class IOSPlatform(override val resourceReader: ResourceReader) : Platform {
-    override val appDataSource: AppDataSource
-        get() = LocalAppDataSource(this)
-}
