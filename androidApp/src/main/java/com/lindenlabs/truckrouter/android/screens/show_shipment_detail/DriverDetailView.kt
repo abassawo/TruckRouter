1a545c167b021ce7f1a19d8f6d29af79f7e4376c
Abass Bayo <abass@Abasss-MBP.lan>
2023-02-27 08:37:49 -0500

    Continued WIP

--- androidApp/src/main/java/com/lindenlabs/truckrouter/android/screens/show_shipment_detail/DriverDetailView.kt
+++ androidApp/src/main/java/com/lindenlabs/truckrouter/android/screens/show_shipment_detail/DriverDetailView.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.navigation.NavController
-import com.lindenlabs.truckrouter.android.screens.show_shipment_detail.map.MapInit
+import com.lindenlabs.truckrouter.android.ui.screens.show_shipment_detail.map.MapInit
 import com.lindenlabs.truckrouter.presentation.ScheduleViewEntity
 
 @Composable
@@ -35,8 +35,8 @@ fun DriverDetailView(
                 .padding(padding)
         ) {
             isLandscape.toTopAppBar(entity, navController)
-            BottomSheet(entity)
-            MapInit()
+//            BottomSheet(entity)
+            MapInit(title = entity.destinationAddress + "\n" + "Suitability score: " + entity.score)
             AndroidView(modifier = Modifier.padding(16.dp), factory = { context ->
                 CoordinatorLayout(context).apply {
                     layoutParams = ViewGroup.LayoutParams(
